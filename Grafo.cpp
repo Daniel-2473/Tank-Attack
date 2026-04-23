f16821ec6d284802d32b5022646ee5c766dd515d
Eythan12345 Mendez Rodriguez <yoelr086@gmail.com>
2026-04-23 16:46:00 -0600

    Commit inicial para inicializar el proyecto + Grafo.cpp + Grafo.h

---
+++ Grafo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by yoelr on 23/4/2026.
+//
+
+#include "Grafo.h"
---
+++ Grafo.h
@@ -0,0 +1,14 @@
+//
+// Created by yoelr on 23/4/2026.
+//
+
+#ifndef TANQUITOS_GRAFO_H
+#define TANQUITOS_GRAFO_H
+
+
+class Grafo
+{
+};
+
+
+#endif //TANQUITOS_GRAFO_H
