08c78f227babe04edb09889af318c963f33813ae
Daniel-2473 <daniel.ulate937@gmail.com>
2026-04-25 09:44:48 -0700

    Estructura de cola simple

---
+++ src/data_estructures/Queue.cpp
@@ -0,0 +1,90 @@
+//
+// Created by danie on 4/24/2026.
+//
+
+#include "Queue.h"
+
+Queue::Queue() {
+    this->arr = new int*[10];
+    this->front = 0;
+    this->rear = 0;
+    this->size = 10;
+    this->used = 0;
+    for (int i = 0; i<size; i++) {
+        arr[i] = nullptr;
+    }
+}
+
+void Queue::Add(int* element) {
+    if (used == 0) {
+        arr[front] = element;
+    }
+    else if (rear == size-1) {
+        if (arr[0] == nullptr) {
+            arr[0] = element;
+            rear = 0;
+        }
+        else {
+            Resize();
+            rear++;
+            arr[rear] = element;
+        }
+    }
+    else {
+        rear++;
+        arr[rear] = element;
+    }
+    used += 1;
+}
+
+int* Queue::Dequeue() {
+    if (used == 0) {
+        return nullptr;
+    }
+    int* element = arr[front];
+    arr[front] = nullptr;
+    if (rear == front) {
+        rear = 0;
+        front = 0;
+    }
+    else {
+        if (front == size-1) {
+            front = 0;
+        }
+        else {
+            front++;
+        }
+    }
+    used--;
+    return element;
+}
+
+void Queue::Resize() {
+    int** newArr = new int*[size + 10];
+    int count  = 0;
+    if (rear >= front) {
+        for (int i = front; i <= rear; i++) {
+            newArr[count] = arr[i];
+            count++;
+        }
+    } else {
+        for (int i = front; i < size; i++) {
+            newArr[count++] = arr[i];
+            count++;
+        }
+        for (int i = 0; i <= rear; i++) {
+            newArr[count] = arr[i];
+            count++;
+        }
+    }
+    size += 10;
+    front = 0;
+    rear = count-1;
+    delete[] arr;
+    arr = newArr;
+    newArr = nullptr;
+}
+
+bool Queue::isEmpty() {
+    return used == 0;
+}
---
+++ src/data_estructures/Queue.h
@@ -0,0 +1,28 @@
+//
+// Created by danie on 4/24/2026.
+//
+
+#ifndef TANQUITOS_QUEUE_H
+#define TANQUITOS_QUEUE_H
+
+
+class Queue {
+public:
+
+    void Add(int* element);
+    Queue();
+    int* Dequeue();
+    bool isEmpty();
+
+private:
+    int** arr;
+    int front;
+    int rear;
+    int used;
+    int size;
+    void Resize();
+
+};
+
+
+#endif //TANQUITOS_QUEUE_H
